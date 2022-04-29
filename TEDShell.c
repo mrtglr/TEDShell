42c308ca9b5f3b7f53068e3f5b1e7368358f82d9
Mert Güler <78295915+mrtglr@users.noreply.github.com>
2022-04-29 19:38:33 +0300

    Update TEDShell.c

--- TEDShell.c
+++ TEDShell.c
@@ -20,14 +20,95 @@
 // To run testcases
 // ./TEDShell-tester
 
+char* yourPath[100];
 
+void initialPath(char *yourPath[])
+{
+    //an array filled with /bin and 99 empty strings
+    yourPath[0] =(char *) malloc(strlen("/bin")+1);
+    strcpy(yourPath[0], "/bin");
+    int i = 0; 
+    for( i = 1; i < 100; i++)
+    {
+        yourPath[i] =(char *) malloc(strlen(" ")+1);
+        strcpy(yourPath[i], " ");     
+    }       
+}
 void readline(char* buffer){
     fgets(buffer, 100, stdin);
     buffer[strcspn(buffer, "\n")] = 0;
 }
 
+int check(char *parsed[], int lenParsed)
+{
+    int isTrue = 0;
+    int i = 1;
+    if(lenParsed == 1)
+    {
+        return 1;
+    }
+    for(i = 1; i < lenParsed; i++)
+    { 
+        int j = 0;
+           
+        for(j = 0; j < 100; j++)
+        { 
+            if(strcmp(yourPath[j]," ") == 0)
+            {
+                break;
+            }
+            else if(strcmp(parsed[i],yourPath[j]) == 0)
+            {
+                isTrue = 1;
+                return isTrue;
+            }
+            else
+            {
+                continue;
+            } 
+        }
+    }
+    return isTrue;
+}
+int path(char* str)
+{
+	int usable = access(str, X_OK);
+	if(usable == -1)
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+	return 0;
+}
+
+void update_path(char * parsed[])
+{
+    int i = 1;
+    while(1)
+    {
+        if(parsed[i] == NULL)
+        {
+            yourPath[i] = NULL;
+            break;
+        }
+        else
+        {
+            if(path(parsed[i])==1)
+            {
+                yourPath[i-1] =(char *) malloc(strlen(parsed[i]) + 1);
+                strcpy(yourPath[i-1], parsed[i]);           
+            }
+        }
+        i++;
+    }
+
+}
+
 // Function to execute builtin commands
-int ownCmdHandler(char** parsed)
+int ownCmdHandler(char** parsed, int lenParsed)
 {
     int numberOfCmd = 3, i, here = 0;
     char* Commands[numberOfCmd];
@@ -39,7 +120,7 @@ int ownCmdHandler(char** parsed)
 	char s[100];
   
     for (i = 0; i < numberOfCmd; i++) {
-        if (parsed[0]!=NULL &&strcmp(parsed[0], Commands[i]) == 0) {
+        if (strcmp(parsed[0], Commands[i]) == 0) {
             here = i + 1;
             break;
         }
@@ -47,79 +128,68 @@ int ownCmdHandler(char** parsed)
 
     switch (here) {
     case 1:
-        if(parsed[1] != NULL){
-            printf("exit cannot take arguments\n");
-            return 1;
-        } else {exit(0);}
+        printf("\nGoodbye\n");
+        exit(0);
     case 2:
     	if(parsed[1] == NULL)
     	{
-            
-    		chdir("/");
-            
+    		printf("An error has occurred\n");
 		}
 		else
 		{
-			// printing current working directory
-	    	printf("before %s\n", getcwd(s, 100));
-	    	// using the command
-	    	int returned = chdir(parsed[1]);
-	    	// printing current working directory
-	    	printf("after %s\n", getcwd(s, 100));
-	    	if(returned < 0)
-	    	{
-	    		perror("chdir() failed");
-			}
+            if(check(parsed, lenParsed) == 1)
+            {
+                // printing current working directory
+                printf("before %s\n", getcwd(s, 100));
+                // using the command
+                int returned = chdir(parsed[1]);
+                // printing current working directory
+                printf("after %s\n", getcwd(s, 100));
+                if(returned < 0)
+                {
+                    perror("chdir() failed");
+                }
+            }
+            else
+            {
+                printf("not in search path\n");
+            }
+
 		}
 	    return 1;
-
+    case 3:
+        if(parsed[1] == NULL)
+    	{
+    		printf("An error has occurred\n");
+		}
+        else
+        {
+            update_path(parsed);
+        }
+        return 0;
     default:
-		//printf("default %s\n", parsed[0]);
         break;
     }
   
     return 0;
 }
 
-void parseCommands(char* str,char** parsed,int* commandNum){
-    
-    int i;
-    
-    for (i = 0; i < 100; i++) {
-        parsed[i] = strsep(&str, "&");
-  		(*commandNum)++;
-        if (parsed[i] == NULL ){
-            (*commandNum)--;
-            break;
-        }
-        if (strlen(parsed[i]) == 0|| parsed[i]==" "){
-            
-            (*commandNum)--;
-            i--;
-        }    
-    }
-}
-
 // function for parsing command words
 void parseSpace(char* str, char** parsed, int* len)
 {
     int i;
-    
     for (i = 0; i < 100; i++) {
         parsed[i] = strsep(&str, " ");
   		(*len)++;
-        if (parsed[i] == NULL ){
+        if (parsed[i] == NULL){
             (*len)--;
             break;
         }
-        if (strlen(parsed[i]) == 0|| parsed[i]==" "){
-            
+        if (strlen(parsed[i]) == 0){
             (*len)--;
             i--;
         }    
     }
-    
-
 }
 int processString(char* str, char** parsed, int* len)
 {
@@ -138,30 +208,22 @@ int processString(char* str, char** parsed, int* len)
   
         parseSpace(str, parsed);
     }*/
-  	parseSpace(str, parsed,len);
-    if (ownCmdHandler(parsed))
-        return 0;
+  	int lenParsed = 0;
+    parseSpace(str, parsed,len);
+    lenParsed = *len;
+    
+    if (ownCmdHandler(parsed, lenParsed))
+        return 1;
+
     else
         //return 1 + piped;
         return 1;
 }
-
-int path(char* str)
+/*int lenghtParsed(int leng)
 {
-	int usable = access(str, X_OK);
-	if(usable == -1)
-	{
-		printf("error in path\n");
-		return 0;
-	}
-	else
-	{
-		
-		printf("true\n");
-		return 1;
-	}
-	return 0;
-}
+    leng = parseSpace(str, parsed,len,leng);
+    return leng;
+}*/
 
 int redirect() {
     int file = open("cout.txt", O_WRONLY | O_CREAT, 0777);
@@ -176,118 +238,110 @@ int redirect() {
 // Function where the system command is executed
 void execArgs(char** parsed,int* len)
 {
+    // Forking a child
+    pid_t pid = fork(); 
+  	
+	int status;
     char *args[2];
 	
-    if((*len)-2>0 && strcmp(parsed[(*len)-2],">")==0 ){
-        int file = open(strcat(parsed[(*len)-1],".txt") , O_WRONLY | O_CREAT, 0777);
-        int file2 = dup2(file,STDOUT_FILENO);
-        close(file);
-    }
+    if (pid == -1) {
+        printf("\nFailed forking child..");
+        return;
+    } 
+	else if (pid == 0) 
+	{  
+        if((*len)-2>0 && strcmp(parsed[(*len)-2],">")==0 ){
+            int file = open(strcat(parsed[(*len)-1],".txt") , O_WRONLY | O_CREAT, 0777);
+            if(file == -1){
+                return;
+            }
+            int file2 = dup2(file,STDOUT_FILENO);
+            close(file);
+        }
 
-    if( parsed[0] != NULL && strcmp(parsed[0], "path") == 0)
-    {
-        printf("girdi pathe");
-        int isPath = path(parsed[1]);
-        if (isPath == 0) 
-        {
-            printf("\nCould not execute command..");
-        }
-        else
-        {
-            args[0] = parsed[1]; 
-            args[1] = NULL;  
-            if(execvp(args[0], args) < 0)
-            {
-                perror("\nCould not execute command..");
-            }
-            else
-            {
-                execv(args[0], args);
-            }
-            exit(0);
-        }
-    }
-    else
-    {
-        if(parsed[0] == NULL)
-        {
-            printf("No input.\n");
-            exit(0);
-        }
-        if(parsed[1] != NULL)
-        {
-            printf("%s\n",parsed[(*len)-2]);
-            
-            
-            //char *test[]={ parsed , NULL};
-            if((*len)-2>0 && strcmp(parsed[(*len)-2],">")==0 ){
-                
-                parsed[(*len)-1]=NULL;
-                parsed[(*len)-2]=NULL;
-                
-                if(execvp(parsed[0], parsed) < 0)
+        
+		if((strcmp(parsed[0], "path") == 0))
+		{
+            printf("path has been updated\n");
+		}
+		else if((strcmp(parsed[0], "cd") != 0))
+		{
+			if(parsed[0] == NULL)
+			{
+				perror("\nNo input.");
+				exit(0);
+			}
+			if(parsed[1] != NULL)
+			{
+                if((*len)-2>0 && strcmp(parsed[(*len)-2],">")==0 ){
+                    
+                    parsed[(*len)-1]=NULL;
+                    parsed[(*len)-2]=NULL;
+                    
+                    if(execvp(parsed[0], parsed) < 0)
+                    {
+                        perror("\nCould not execute command..");
+                    } 
+                }
+                else
                 {
-                    perror("\nCould not execute command..");
-                } 
-            }else{
-                
-                if(execvp(parsed[0], parsed) < 0)
-                {
-                    perror("\nCould not execute command..");
-                } 
-            }
-            
-            
-            exit(0);
-        }
-        else
-        {
-            args[0] = "/bin/ls"; 
-            args[1] = NULL;
-            //printf("%s    %s\n",parsed[0], parsed[1]);
-            if(execvp(parsed[0], parsed) < 0)
-            {
-                perror("\nCould not execute command..[parsed[1] nullsa]");
-            } 
-            
-            exit(0);
-        }			
-    }
+                    if(check(parsed, *len) == 1)
+                    {
+                        if(execvp(parsed[0], parsed) < 0)
+                        {
+                            perror("\nCould not execute command..");
+                        } 
+                    }
+                    else
+                    {
+                        printf("not in search path\n");
+                    }
 
-    exit(0);
-    
-	
+                }
+
+				exit(0);
+			}
+			else
+			{
+                char p[100];
+                getcwd(p, 100);
+                char *temp[2];
+                temp[0] = p;
+                temp[1] = p;
+                if(check(temp,*len) == 1)
+                {                   
+                    if(execvp(parsed[0], parsed) < 0)
+                    {
+                        perror("\nCould not execute command..[parsed[1] nullsa]");
+                    } 
+                }
+                else
+                {
+                    perror("not in search path\n");
+                }       
+				exit(0);
+			}			
+		}
+
+        exit(0);
+    } 
+	else 
+	{
+        // waiting for child to terminate 
+		wait(&status);
+        //wait(NULL); 
+        return;
+    }
 }
 
 void executeCmds(char input[], char *parsedArgs[])
 {
     int isExe = 0;
     int len=0;
-    int commandNum=0;
-    char* parsedCommands[100];
-
-    parseCommands(input,parsedCommands,&commandNum);
-
-
-    
-    for(int i=0; i<commandNum;i++){
-    
-    isExe = processString(parsedCommands[i],parsedArgs,&len);
-    
-        if(isExe == 1)
-        {
-            if(fork()==0){
-            execArgs(parsedArgs,&len);
-            exit(0);
-            }
-        }
-    }
-    int status;
-    for (int i = 0; i < commandNum; ++i)
+    isExe = processString(input,parsedArgs,&len);
+    if(isExe == 1)
     {
-
-        wait(&status);
-        
+        execArgs(parsedArgs,&len);
     }
 }
 
@@ -319,8 +373,15 @@ int batch(char* fileName) //cd /mnt/c/users/hmert/desktop/unix
 int main(int argc, char *argv[])
 {
     char *parsedArgs[100];
-    char input[MAXINPUT + 1];
 
+    int i = 0; 
+    for( i = 1; i < 100; i++)
+    {
+        yourPath[i] =(char *) malloc(strlen(" ")+1);
+        strcpy(yourPath[i], " ");     
+    }   
+    char input[MAXINPUT + 1];
+    initialPath(yourPath);
     if(argc >1){
         batch(argv[1]);
     }
@@ -329,7 +390,7 @@ int main(int argc, char *argv[])
         printf("TEDshell> ");
         readline(input);
 
-        while (1)
+        while (strcmp(input, "exit") != 0)
         {
 
             executeCmds(input,parsedArgs);
